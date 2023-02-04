68b90e75db16ee7485c01b75b07203bdc735b4b0
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-02-04 00:23:37 -0800

    Edit db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connect-URI.
-const mongoURI = "";
+const mongoURI = "mongodb+srv://MuhammadShahzaib:MSA123@cluster0.nmvvgl4.mongodb.net/RidesApp?retryWrites=true&w=majority";
 
 // Connect-to-mongodb
 mongoose.connect(mongoURI);
